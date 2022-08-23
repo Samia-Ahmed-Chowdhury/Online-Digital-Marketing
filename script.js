517e4fa38d8ed7cf4cd490dcee56d3b60aa5b6be
Samia-Ahmed-Chowdhury <samiachowdhury546@gmail.com>
2022-08-23 11:34:22 +0600

    samia

---
+++ script.js
@@ -0,0 +1,11 @@
+var form = document.getElementById("form");
+function show(x) {
+    x.style.display="none";
+   form.style.display="block";
+  }
+  
+  function hide(x) {
+    x.style.display="block";
+   form.style.display = "none";
+    
+  }
