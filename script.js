161940b553aa294d51aa5cab7c98ba2f11853185
Samia-Ahmed-Chowdhury <samiachowdhury546@gmail.com>
2022-08-25 08:08:48 +0600

    commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//form on hover
 var form = document.getElementById("form");
 function show(x) {
     x.style.display="none";
@@ -9,3 +11,25 @@ function show(x) {
    form.style.display = "none";
     
   }
+
+
+
+  //counter success
+
+  
+// Countdown Success
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue + '+';
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
